09b5adbd216343022981a5740ff92dee75071e4e
Sarah Yusufali <yusufals@aston.ac.uk>
2018-09-14 11:07:08 +0100

    I created a new class - PlayerStorage and created a database of players

---
+++ src/UNO/PlayerStorage.java
@@ -0,0 +1,53 @@
+package UNO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yusufals on 14/09/2018.
+ */
+public class PlayerStorage {
+
+    List<Player> playerDatabase = new ArrayList<Player>();
+
+
+
+    public PlayerStorage(){
+
+        Player player1= new Player();
+        Player player2= new Player();
+        Player player3= new Player();
+        Player player4= new Player();
+        Player player5= new Player();
+        Player player6= new Player();
+        Player player7= new Player();
+        Player player8= new Player();
+        Player player9= new Player();
+        Player player10= new Player();
+
+
+        playerDatabase.add(player1);
+        playerDatabase.add(player2);
+        playerDatabase.add(player3);
+        playerDatabase.add(player4);
+        playerDatabase.add(player5);
+        playerDatabase.add(player6);
+        playerDatabase.add(player7);
+        playerDatabase.add(player8);
+        playerDatabase.add(player9);
+        playerDatabase.add(player10);
+
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
--- src/UNO/Rules.java
+++ src/UNO/Rules.java
@@ -1,5 +1,7 @@
 package UNO;
 
+import Exceptions.NoCardRemainingException;
+
 /**
  * Created by capranom on 12/09/2018.
  */
@@ -8,7 +10,7 @@ public class Rules{
     Player player;
     UnoGame game;
 
-   public Rules(){
+   public Rules() throws NoCardRemainingException {
         player = new Player();
         game = new UnoGame();
     }
